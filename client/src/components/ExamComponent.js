1b011f84d7d1f1fd13842eb790d8e0243068cc3f
Anurag Tiwari <anurag1504t@gmail.com>
2020-08-22 00:01:36 +0530

    Vulnerability Fixed

--- client/src/components/ExamComponent.js
+++ client/src/components/ExamComponent.js
@@ -12,6 +12,8 @@ const RenderExamItem = ({ exam }) => {
             <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
                 <CardTitle>{exam.title}</CardTitle>
                 <CardText>Course Code: {exam.courseCode} <br></br> {exam.description}</CardText>
+                <CardText>Start Time: {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(exam.start)))}</CardText>
+                <CardText>End Time: {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day:'2-digit'}).format(new Date(Date.parse(exam.end)))}</CardText>
                 <Link to={`/exams/${exam._id}`} >
                     <Button>Start</Button>
                 </Link>                
