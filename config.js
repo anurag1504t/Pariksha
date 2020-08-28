8249f0630492ba13a9bdba1e5fb90ffac05243df
anurag1504t <64521184+anurag1504t@users.noreply.github.com>
2020-08-28 19:25:34 +0530

    Delete config.js

--- config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb+srv://Anurag:54GjMJFmLG3EaLF7@pariksha.fzjja.mongodb.net/pariksha?retryWrites=true&w=majority'
-}
