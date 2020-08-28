cbd66f6a710ec43b0f49342b3377d4fa04a34aec
Anurag Tiwari <anurag1504t@gmail.com>
2020-08-28 19:22:11 +0530

    Updated .gitignore

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/Pariksha'
+    'mongoUrl' : 'mongodb+srv://Anurag:54GjMJFmLG3EaLF7@pariksha.fzjja.mongodb.net/pariksha?retryWrites=true&w=majority'
 }
